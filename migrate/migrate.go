ac0def4db3962cb06a8a7c0b262988aceb8448e0
pokotieru-mashumaro <k827umeboshi@gmail.com>
2025-01-03 15:39:27 +0900

    db migrateまで

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"REST_golang/db"
+	"REST_golang/model"
+	"fmt"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
